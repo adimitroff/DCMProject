8282606f97ccfd61a6b7699ff8871f032c2ee0f8
KrumSt <krum.stoilov@gmail.com>
2017-10-27 18:30:07 +0300

    Added commands and js files to the protocol for device data response.
    Add test media files to project WebContent/media/
    Url to test device response http://localhost:8660/DCMProject/devfeed

--- DCMProject/src/net/cb/dcm/dev_feed/DevFeeder.java
+++ DCMProject/src/net/cb/dcm/dev_feed/DevFeeder.java
@@ -14,90 +14,100 @@ import org.slf4j.LoggerFactory;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import net.cb.dcm.dev_feed.DevResponseData.ResponseDataType;
+import net.cb.dcm.enums.DevResponseDataType;
 import net.cb.dcm.enums.MediaObjectType;
 import net.cb.dcm.jpa.DeviceDAO;
 import net.cb.dcm.jpa.entities.Device;
 import net.cb.dcm.jpa.entities.MediaContent;
 
 /**
- * Servlet for processing http request for new content from the samsung tv app and other smart devices.
+ * Servlet for processing http request for new content from the samsung tv app
+ * and other smart devices.
  */
 public class DevFeeder extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private static final Logger moLogger = LoggerFactory.getLogger(DevFeeder.class);
+	private static final long serialVersionUID = 1L;
+	private static final Logger moLogger = LoggerFactory.getLogger(DevFeeder.class);
 
-    /** {@inheritDoc} */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	String loIp = request.getParameter("ip");
-		
-    	if (loIp == null || loIp.isEmpty()) {
-    		this.getDefaultPlayList(response);
-    	} else {
-    		try {
-    			response.getWriter().println("<p>Find the device with IP: " + loIp + " and generate the page</p>");
-    			DeviceDAO loDeviceDao = new DeviceDAO();
-    			Device loDevice = loDeviceDao.findDeviceByIp(loIp);
-    			response.getWriter().println("<p> loDevice: " + loDevice + " </p>" );
-    			if (loDevice == null || loDevice.getId() <= 0){
-    				response.getWriter().println("<p> Register new device</p>");
-    				//// Register device ID in the database
-    				//// Maybe registering only the IP, then the system opearator should see it in the 
-    				//// list as a device without data, and maintain it. 
-    				loDevice = loDeviceDao.registerNewDevice(loIp);
-    				moLogger.debug("loDevice id: " + loDevice.getId());
-    				////    				Load device default playlist
-    			} else {
-    				////    				Get device playlist and load it  
-    			}
-    		} catch (Exception e){
-    			moLogger.error("Error rendering the page: " + e.getMessage());
-    			throw new ServletException(e);
-    		}
-    		
-    	}
-    }
+	/** {@inheritDoc} */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String loIp = request.getParameter("ip");
+		String lsServerUrl = "http://" + request.getServerName() + ":" + request.getServerPort();
+		if (loIp == null || loIp.isEmpty()) {
+			moLogger.debug("No device IP. Generating default response.");
+			this.getDefaultPlayList(lsServerUrl, response);
+		} else {
+			DeviceDAO loDeviceDao = new DeviceDAO();
+			Device loDevice = loDeviceDao.findDeviceByIp(loIp);
+			if (loDevice == null || loDevice.getId() <= 0) {
+				moLogger.debug("Register new device with IP " + loIp);
+				//// Register device ID in the database
+				//// Maybe registering only the IP, then the system
+				//// opearator should see it in the
+				//// list as a device without data, and maintain it.
+				loDevice = loDeviceDao.registerNewDevice(loIp);
+				// Load device default playlist
+				this.getDefaultPlayList(lsServerUrl, response);
+			} else {
+				// TODO Get device playlist and load it
+				this.getDefaultPlayList(lsServerUrl, response);
+			}
+		}
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-            IOException {
-//    	Should not be called
-    	doGet(request, response);
-    }
-    
-    private void getDefaultPlayList(HttpServletResponse response) throws IOException
-    {
+	/** {@inheritDoc} */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Should not be called
+		doGet(request, response);
+	}
+
+	/**
+	 * Generates default json data with play list files for the device
+	 * 
+	 * @param serverUrl
+	 *            Server url including port number
+	 * @param response
+	 *            - Response to write json data
+	 * @throws IOException
+	 */
+	private void getDefaultPlayList(String serverUrl, HttpServletResponse response) throws IOException {
 		Gson loGson = new GsonBuilder().setPrettyPrinting().create();
-		DevResponseData devResponseData = new DevResponseData(null);
-		devResponseData.setResponseDataType(ResponseDataType.DATA_TYPE_PLAY_LIST);
+		DevResponse loDevResponse = new DevResponse(serverUrl);
+		loDevResponse.setResponseDataType(DevResponseDataType.PLAY_LIST);
 		ArrayList<MediaContent> mediaContents = new ArrayList<>();
-		
+
 		//////////////////////////////////
 		// Start adding some testing contents for the playlist
 		MediaContent mediaContent = new MediaContent();
 		mediaContent.setMediaType(MediaObjectType.MPEG);
 		mediaContent.setName("MtelDTH.mp4");
 		mediaContents.add(mediaContent);
-		
+
+		mediaContent = new MediaContent();
+		mediaContent.setMediaType(MediaObjectType.JPEG);
+		mediaContent.setName("map4g.jpg");
+		mediaContents.add(mediaContent);
+
 		mediaContent = new MediaContent();
 		mediaContent.setMediaType(MediaObjectType.MPEG);
 		mediaContent.setName("MtelUniveralPrima2.m4v");
 		mediaContents.add(mediaContent);
-		
+
 		mediaContent = new MediaContent();
 		mediaContent.setMediaType(MediaObjectType.MPEG);
 		mediaContent.setName("MTELHomePhoneTVC.mp4");
 		mediaContents.add(mediaContent);
-		
-		devResponseData.setMediaContents(mediaContents);
+
+		loDevResponse.setMediaContents(mediaContents);
 		// End adding of testing content
 		///////////////////////////////
-		
-		devResponseData.generateResponse();
-	    response.setContentType("application/json");
-	    response.setCharacterEncoding("UTF-8");
-	    response.getWriter().write(loGson.toJson(devResponseData));
-    }
+
+		loDevResponse.generateResponse();
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(loGson.toJson(loDevResponse.getResponseJson()));
+	}
 }
