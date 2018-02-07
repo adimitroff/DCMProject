95378fee769b2533622555fd14860c9e5b973c77
iko <ilia_it@abv.bg>
2018-02-07 13:20:59 +0200

    Merge

--- DCMProject/WebContent/dcmweb/controller/add_playlist.controller.js
+++ DCMProject/WebContent/dcmweb/controller/add_playlist.controller.js
@@ -159,6 +159,7 @@ sap.ui
 						this.getView().byId("priority").setValue("");
 						this.getView().byId("idValidFrom").setValue("");
 						this.getView().byId("idValidTo").setValue("");
+						
 					},
 					formatSelected: function(ivValue){
 						return true;
